d66317e252ae65f5072e5733338e8e5e72fe9c95
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2023-12-28 08:49:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH", "fcdc178451cd234e63faefd38895c991")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6435409185:AAFYzxyD--pYrS5sjDqPEecRmlzrnD7XcRI")
-    TELEGRAM_API = 1923471
-    OWNER = os.environ.get("OWNER", "880087645")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "jasuran2p0")
+    API_HASH = os.environ.get("API_HASH", "f19aed00b0c74abed0359016afc1733f")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6855063215:AAEBzl6S1kegerTv-jyUgP4Q_PdKqtU5G74")
+    TELEGRAM_API = 8733404
+    OWNER = os.environ.get("OWNER", "807374433")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "NewUserExist")
     PASSWORD = os.environ.get("PASSWORD", "asuran")
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = os.environ.get("LOGCHANNEL", "-1002119311182")  # Add channel id as -100 + Actual ID
